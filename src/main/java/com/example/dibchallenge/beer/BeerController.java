6899bc4cc106e520c7b06ba7079aab280285d298
Stefan Golubovic <stefan.golubovic@gmx.com>
2020-02-19 20:18:46 +0100

    Fix typo. Setup REST API.

--- src/main/java/com/example/dibchallenge/beer/BeerController.java
+++ src/main/java/com/example/dibchallenge/beer/BeerController.java
@@ -1,7 +1,51 @@
 package com.example.dibchallenge.beer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/beers")
 public class BeerController {
+
+    private final BeerService beerService;
+
+    @Autowired
+    public BeerController(BeerService beerService) {
+        this.beerService = beerService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Page<BeerDto>> getAll(
+            @PageableDefault(page = 0, size = 50)
+            @SortDefault(sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BeerDto> getById(@PathVariable("id") Long id) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable("id") Long id) {
+        return null;
+    }
+
+    @PostMapping("/insert")
+    public ResponseEntity<Long> insertBeers(
+            @RequestParam(value = "numberOfBeers", required = false, defaultValue = "10") Long numberOfBeers) {
+        return null;
+    }
 }
--- src/main/java/com/example/dibchallenge/beer/BeerRepository.java
+++ src/main/java/com/example/dibchallenge/beer/BeerRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BeerRepository extends JpaRepository<BeerRepository, Long> {
+public interface BeerRepository extends JpaRepository<BeerEntity, Long> {
 }
